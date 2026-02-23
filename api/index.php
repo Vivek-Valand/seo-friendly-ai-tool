a1c7913ff5e425269f69786ea8c13bb88b03dced
VIVEK VALAND <131301501+Vivek-Valand@users.noreply.github.com>
2026-02-23 12:22:46 +0530

    update api/index file

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-// api/index.php
+// Ensure the /tmp directory exists for Laravel views
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';

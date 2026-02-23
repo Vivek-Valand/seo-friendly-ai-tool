62e707319bfa31a248acca4a0d4bbeefd6d3ba3a
VIVEK VALAND <131301501+Vivek-Valand@users.noreply.github.com>
2026-02-23 12:39:03 +0530

    add migration route

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,13 @@ Route::get('/chat/sidebar-history', [ChatController::class, 'historyPartial'])->
 Route::get('/chat/{id}', [ChatController::class, 'show'])->name('chat.show');
 Route::delete('/chat/{id}', [ChatController::class, 'destroy'])->name('chat.destroy');
 Route::post('/chat/send', [ChatController::class, 'send'])->name('chat.send');
+
+Route::get('/migrate-db', function () {
+    try {
+        // This runs 'php artisan migrate --force'
+        Artisan::call('migrate', ["--force" => true]);
+        return "Database migration successful! <br><br> Logs: <br>" . Artisan::output();
+    } catch (\Exception $e) {
+        return "Migration failed: " . $e->getMessage();
+    }
+});
